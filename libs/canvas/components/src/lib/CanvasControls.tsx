1c039425da0ecf0582ae7f3eebe7483a5c8f7930
mlefebvre-amalia <maxence@amalia.io>
2024-01-04 09:07:18 +0100

    feat: better control bar

--- libs/canvas/components/src/lib/CanvasControls.tsx
+++ libs/canvas/components/src/lib/CanvasControls.tsx
@@ -1,4 +1,5 @@
 import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import { useViewportContext } from '@feyroads/editor/viewport/state';
 import { useKeyboard } from '@feyroads/ext/react/hooks';
 import { useDebugModeContext } from '@feyroads/kernel/debug';
@@ -6,8 +7,61 @@ import { useGraphStateContext } from '@feyroads/math/state';
 import { IconDeviceFloppy, IconTool, IconTrash } from '@tabler/icons-react';
 import { ComponentPropsWithoutRef, memo, useCallback } from 'react';
 
-
 export type CanvasControlsProps = ComponentPropsWithoutRef<'div'>;
+
+export const ControlButton = styled.button`
+  position: relative;
+
+  all: unset;
+
+  --border-radius: 0.25rem;
+  --size: 2.5rem;
+
+  --hover-bg-color: #e5e4ff;
+  --hover-color: #4946bd;
+
+  border-radius: var(--border-radius);
+  display: inline-flex;
+  position: relative;
+  cursor: pointer;
+
+  &:hover {
+    background-color: var(--hover-bg-color);
+
+    * {
+      color: var(--hover-color);
+    }
+  }
+`;
+
+export const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border-radius: var(--border-radius);
+  width: var(--size);
+  height: var(--size);
+`;
+
+// export const KeyBinding = styled.span`
+//   position: absolute;
+//   bottom: 4px;
+//   right: 4px;
+//
+//   color: #b8b8b8;
+//   font-size: 0.75rem;
+//
+//   text-size-adjust: 100%;
+// `;
+
+export const icon = css`
+  --color: #1b1b1f;
+
+  width: calc(var(--size) / 2);
+  height: calc(var(--size) / 2);
+  color: var(--color);
+`;
 
 export const CanvasControls = memo(function CanvasControls({
   ...props
@@ -27,19 +81,35 @@ export const CanvasControls = memo(function CanvasControls({
     <div
       {...props}
       css={css`
-        display: flex;
-        gap: 20px;
+        display: grid;
+        grid-auto-flow: column;
+        grid-auto-columns: min-content;
+        grid-template-rows: auto;
+
+        gap: 0.25rem;
       `}
     >
-      <button onClick={onClickSave} title="Save graph to local storage">
-        <IconDeviceFloppy />
-      </button>
-      <button onClick={disposeGraph} title="Reset graph to default">
-        <IconTrash />
-      </button>
-      <button onClick={toggleDebugMode} title="Toggle debug mode">
-        <IconTool />
-      </button>
+      <ControlButton
+        onClick={onClickSave}
+        title="Save graph to local storage -- ctrl + s"
+      >
+        <IconContainer>
+          <IconDeviceFloppy css={icon} />
+        </IconContainer>
+      </ControlButton>
+      <ControlButton onClick={disposeGraph} title="Reset graph to default">
+        <IconContainer>
+          <IconTrash css={icon} />
+        </IconContainer>
+      </ControlButton>
+      <ControlButton
+        onClick={toggleDebugMode}
+        title="Toggle debug mode -- ctrl + d"
+      >
+        <IconContainer>
+          <IconTool css={icon} />
+        </IconContainer>
+      </ControlButton>
     </div>
   );
 });
